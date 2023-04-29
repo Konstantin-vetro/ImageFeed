4f547840741cb8dfbb02d9377af958e1c47151ad
Konstantin-vetro <konstantin.vetro@yandex.ru>
2023-04-29 22:46:41 +0500

    Создал класс для ячейки с идентификатором

---
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageListCell.swift
+//  ImageFeed
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+    
+}
